2f035c08983afb424b1f7f0025c643d5b5fbf5fb
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2019-04-21 23:57:08 +0500

    Wrote class for caching patterns

---
+++ SFNT/PatternCache.swift
@@ -0,0 +1,58 @@
+//
+// Copyright (C) 2019 Muhammad Tayyab Akram
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+
+struct PatternKey: Hashable {
+    let scriptTag: SFNTTag
+    let languageTag: SFNTTag
+}
+
+fileprivate class PatternValue {
+    let sfPattern: SFPatternRef
+
+    init(_ sfPattern: SFPatternRef) {
+        self.sfPattern = SFPatternRetain(sfPattern)
+    }
+
+    deinit {
+        SFPatternRelease(sfPattern)
+    }
+}
+
+class PatternCache {
+    private let semaphore = DispatchSemaphore(value: 1)
+    private var patterns = Dictionary<PatternKey, PatternValue>()
+
+    subscript(key: PatternKey) -> SFPatternRef? {
+        get {
+            semaphore.wait()
+            defer { semaphore.signal() }
+
+            if let index = patterns.index(forKey: key) {
+                return patterns[index].value.sfPattern
+            }
+
+            return nil
+        }
+        set {
+            semaphore.wait()
+            defer { semaphore.signal() }
+
+            patterns[key] = newValue != nil ? PatternValue(newValue!) : nil
+        }
+    }
+}
