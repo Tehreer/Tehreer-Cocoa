f08bd404a06ef63221668069a0e8bf28ee8a6334
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2019-05-14 20:10:30 +0500

    Wrote methods related to measurement and hit testing in glyph run

--- Layout/GlyphRun.swift
+++ Layout/GlyphRun.swift
@@ -76,6 +76,15 @@ public class GlyphRun {
         return glyphIDs.count
     }
 
+    public var width: CGFloat {
+        // Locking is not required for constant width.
+        if extent == -CGFloat.infinity {
+            extent = computeTypographicExtent(forGlyphRange: 0 ..< glyphIDs.count)
+        }
+
+        return extent
+    }
+
     public var height: CGFloat {
         return ascent + descent + leading
     }
@@ -124,4 +133,100 @@ public class GlyphRun {
 
         return Clusters.trailingGlyphIndex(in: clusterMap, for: arrayIndex, isBackward: isBackward, glyphCount: glyphCount)
     }
+
+    private func caretEdge(forCharacterAt index: String.Index) -> CGFloat {
+        let extraStart = utf16.index(startIndex, offsetBy: -startExtraLength)
+        let arrayIndex = utf16.distance(from: extraStart, to: index)
+
+        return caretEdges[arrayIndex]
+    }
+
+    private func leadingEdge(forCharacterRange range: Range<String.Index>) -> CGFloat {
+        return caretEdge(forCharacterAt: !isBackward ? range.lowerBound : range.upperBound)
+    }
+
+    public func distanceForCharacter(at index: String.Index) -> CGFloat {
+        checkCharacterIndex(index)
+
+        return caretEdge(forCharacterAt: index)
+    }
+
+    func distanceForCharacters(in range: Range<String.Index>) -> CGFloat {
+        let extraStart = utf16.index(startIndex, offsetBy: -startExtraLength)
+
+        let lowerBound = utf16.distance(from: extraStart, to: range.lowerBound)
+        let upperBound = utf16.distance(from: extraStart, to: range.upperBound)
+
+        let lowerEdge = caretEdges[lowerBound]
+        let upperEdge = caretEdges[upperBound]
+
+        return isRTL ? lowerEdge - upperEdge : upperEdge - lowerEdge
+    }
+
+    public func nearestCharacterIndex(at distance: CGFloat) -> String.Index {
+        let extraStart = utf16.index(startIndex, offsetBy: -startExtraLength)
+
+        var leadingCharIndex: String.Index?
+        var trailingCharIndex: String.Index?
+
+        var leadingCaretEdge: CGFloat = 0.0
+        var trailingCaretEdge: CGFloat = 0.0
+
+        var index = isRTL ? endIndex : startIndex
+        let next = isRTL ? -1 : 1
+
+        while index <= endIndex && index >= startIndex {
+            let caretEdge = caretEdges[utf16.distance(from: extraStart, to: index)]
+
+            if caretEdge <= distance {
+                leadingCharIndex = index
+                leadingCaretEdge = caretEdge
+            } else {
+                trailingCharIndex = index
+                trailingCaretEdge = caretEdge
+                break
+            }
+
+            index = utf16.index(index, offsetBy: next)
+        }
+
+        if leadingCharIndex == nil {
+            // No char is covered by the input distance.
+            return startIndex
+        }
+
+        if trailingCharIndex == nil {
+            // Whole run is covered by the input distance.
+            return endIndex
+        }
+
+        if distance <= (leadingCaretEdge + trailingCaretEdge) / 2.0 {
+            // Input distance is closer to first edge.
+            return leadingCharIndex!
+        }
+
+        // Input distance is closer to second edge.
+        return trailingCharIndex!
+    }
+
+    public func computeBoundingBox(forGlyphRange range: Range<Int>, using renderer: Renderer) -> CGRect {
+        renderer.typeface = typeface
+        renderer.typeSize = typeSize
+        renderer.writingDirection = writingDirection
+
+        return renderer.computeBoundingBox(glyphIDs: glyphIDs[range],
+                                           offsets: glyphOffsets[range],
+                                           advances: glyphAdvances[range])
+    }
+
+    public func computeTypographicExtent(forGlyphRange range: Range<Int>) -> CGFloat {
+        var extent: CGFloat = 0.0
+
+        for i in range {
+            extent += glyphAdvances[i]
+        }
+
+        return extent
+    }
+
 }
