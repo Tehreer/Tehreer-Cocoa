7d821ddee4fb6a39d1310d3f68fc51c2f4c3e1de
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2019-07-06 15:50:32 +0500

    Added xcode projects

---
+++ Source/BridgingHeader.h
@@ -0,0 +1,30 @@
+//
+// Copyright (C) 2019 Muhammad Tayyab Akram
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include <ft2build.h>
+#include FT_ADVANCES_H
+#include FT_FREETYPE_H
+#include FT_SFNT_NAMES_H
+#include FT_SIZES_H
+#include FT_STROKER_H
+#include FT_SYSTEM_H
+#include FT_TRUETYPE_TABLES_H
+#include FT_TYPES_H
+
+#include <linebreak.h>
+
+#include <SheenBidi.h>
+#include <SheenFigure.h>
