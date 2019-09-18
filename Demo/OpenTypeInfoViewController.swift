63878da5237ce395cefeb69f07c919b4d62983b9
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2019-09-18 22:52:58 +0500

    [demo] Added view controller for displaying text shaping info

---
+++ Demo/OpenTypeInfoViewController.swift
@@ -0,0 +1,193 @@
+//
+// Copyright (C) 2019 Muhammad Tayyab Akram
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import TehreerCocoa
+import UIKit
+
+private struct ClusterDetail {
+    let codePoints: [UnicodeScalar]
+    let glyphIDs: PrimitiveCollection<UInt16>
+    let glyphOffsets: PrimitiveCollection<CGPoint>
+    let glyphAdvances: PrimitiveCollection<CGFloat>
+}
+
+private struct TextDetail {
+    let string: String
+    let glyphIDs: PrimitiveCollection<UInt16>
+    let glyphOffsets: PrimitiveCollection<CGPoint>
+    let glyphAdvances: PrimitiveCollection<CGFloat>
+    let clusterMap: PrimitiveCollection<Int>
+    let clusterInitials: [Int]
+
+    var clusterCount: Int {
+        return clusterInitials.count - 1
+    }
+
+    func clusterDetail(at index: Int) -> ClusterDetail {
+        let codeUnitStart = clusterInitials[index]
+        let codeUnitEnd = clusterInitials[index + 1]
+
+        let characterStart = string.utf16.index(string.startIndex, offsetBy: codeUnitStart)
+        let characterEnd = string.utf16.index(string.startIndex, offsetBy: codeUnitEnd)
+        let clusterString = string[characterStart ..< characterEnd]
+
+        let glyphStart = clusterMap[codeUnitStart]
+        let glyphEnd = (codeUnitEnd < clusterMap.count ? clusterMap[codeUnitEnd] : glyphIDs.count)
+        let glyphRange = glyphStart ..< glyphEnd
+
+        return ClusterDetail(codePoints: Array(clusterString.unicodeScalars),
+                             glyphIDs: glyphIDs[glyphRange],
+                             glyphOffsets: glyphOffsets[glyphRange],
+                             glyphAdvances: glyphAdvances[glyphRange])
+    }
+}
+
+class CharacterDetailCell: UITableViewCell {
+    @IBOutlet weak var characterLabel: UILabel!
+}
+
+class GlyphDetailCell: UITableViewCell {
+    @IBOutlet weak var glyphIDLabel: UILabel!
+    @IBOutlet weak var glyphOffsetLabel: UILabel!
+    @IBOutlet weak var glyphAdvanceLabel: UILabel!
+}
+
+class ClusterDetailCell: UITableViewCell, UITableViewDataSource {
+    @IBOutlet private weak var charactersTableView: UITableView!
+    @IBOutlet private weak var glyphsTableView: UITableView!
+    @IBOutlet private weak var charactersHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var glyphsHeightConstraint: NSLayoutConstraint!
+
+    private var clusterDetail: ClusterDetail!
+
+    fileprivate func configure(with clusterDetail: ClusterDetail) {
+        self.clusterDetail = clusterDetail
+
+        NSLayoutConstraint.deactivate([
+            charactersHeightConstraint, glyphsHeightConstraint
+        ])
+
+        charactersTableView.reloadData()
+        glyphsTableView.reloadData()
+
+        charactersTableView.layoutIfNeeded()
+        glyphsTableView.layoutIfNeeded()
+
+        charactersHeightConstraint.constant = charactersTableView.contentSize.height
+        glyphsHeightConstraint.constant = glyphsTableView.contentSize.height
+
+        NSLayoutConstraint.activate([
+            charactersHeightConstraint, glyphsHeightConstraint
+        ])
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch tableView {
+        case charactersTableView:
+            return clusterDetail.codePoints.count
+
+        case glyphsTableView:
+            return clusterDetail.glyphIDs.count
+
+        default:
+            fatalError()
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch tableView {
+        case charactersTableView:
+            let codePoint = clusterDetail.codePoints[indexPath.row]
+
+            let cell = tableView.dequeueReusableCell(withIdentifier: "\(CharacterDetailCell.self)", for: indexPath) as! CharacterDetailCell
+            cell.characterLabel.text = String(format: "\u{2066}%04X (\(Character(codePoint)))", codePoint.value)
+
+            return cell
+
+        case glyphsTableView:
+            let glyphID = clusterDetail.glyphIDs[indexPath.row]
+            let glyphOffset = clusterDetail.glyphOffsets[indexPath.row]
+            let glyphAdvance = clusterDetail.glyphAdvances[indexPath.row]
+
+            let cell = tableView.dequeueReusableCell(withIdentifier: "\(GlyphDetailCell.self)", for: indexPath) as! GlyphDetailCell
+            cell.glyphIDLabel.text = String(format: "%04X", glyphID)
+            cell.glyphOffsetLabel.text = "(\(Int(glyphOffset.x + 0.5)), \(Int(glyphOffset.y + 0.5)))"
+            cell.glyphAdvanceLabel.text = "\(Int(glyphAdvance + 0.5))"
+
+            return cell
+
+        default:
+            fatalError()
+        }
+    }
+}
+
+class OpenTypeInfoViewController: UITableViewController {
+    private var textDetail: TextDetail!
+
+    func setup(typeface: Typeface, typeSize: Int,
+               scriptTag: SFNTTag, languageTag: SFNTTag, sourceText: String) {
+        let shapingEngine = ShapingEngine()
+        shapingEngine.typeface = typeface
+        shapingEngine.typeSize = CGFloat(typeSize)
+        shapingEngine.scriptTag = scriptTag
+        shapingEngine.languageTag = languageTag
+
+        let textRange = sourceText.startIndex ..< sourceText.endIndex
+        let shapingResult = shapingEngine.shape(text: sourceText, range: textRange)
+        let clusterMap = shapingResult.clusterMap
+        let length = clusterMap.count
+
+        var initials: [Int] = [0]
+        var previous = -1
+
+        for i in 0 ..< length {
+            let value = clusterMap[i]
+            if value != previous {
+                initials.append(i)
+            }
+
+            previous = value
+        }
+        initials.append(length)
+
+        textDetail = TextDetail(string: sourceText,
+                                glyphIDs: shapingResult.glyphIDs,
+                                glyphOffsets: shapingResult.glyphOffsets,
+                                glyphAdvances: shapingResult.glyphAdvances,
+                                clusterMap: shapingResult.clusterMap,
+                                clusterInitials: initials)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return textDetail.clusterCount
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "\(ClusterDetailCell.self)", for: indexPath) as! ClusterDetailCell
+        cell.configure(with: textDetail.clusterDetail(at: indexPath.row))
+
+        return cell
+    }
+}
--- Demo/OpenTypeShapingViewController.swift
+++ Demo/OpenTypeShapingViewController.swift
@@ -19,6 +19,7 @@ import UIKit
 
 private enum SegueID {
     static let showTypefaces = "ShowTypefaces"
+    static let showInfo = "ShowInfo"
 }
 
 class OpenTypeShapingViewController: UITableViewController {
@@ -28,7 +29,7 @@ class OpenTypeShapingViewController: UITableViewController {
     @IBOutlet private weak var languageTagField: UITextField!
     @IBOutlet private weak var inputTextView: UITextView!
 
-    private var selectedTypeface = TypefaceManager.shared.availableTypefaces.first
+    private var selectedTypeface: Typeface! = TypefaceManager.shared.availableTypefaces.first
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,19 +41,67 @@ class OpenTypeShapingViewController: UITableViewController {
         typefaceTitleLabel.text = selectedTypeface?.familyName
     }
 
+    private func textOrPlaceholder(of textField: UITextField) -> String {
+        let text = textField.text ?? ""
+
+        if text.isEmpty {
+            return textField.placeholder ?? ""
+        }
+
+        return text
+    }
+
+    // MARK: - Properties
+
+    private var typeSize: Int! {
+        return Int(textOrPlaceholder(of: typeSizeField))
+    }
+
+    private var scriptTag: SFNTTag! {
+        return SFNTTag(string: textOrPlaceholder(of: scriptTagField))
+    }
+
+    private var languageTag: SFNTTag! {
+        return SFNTTag(string: textOrPlaceholder(of: languageTagField))
+    }
+
+    private var inputText: String {
+        return inputTextView.text
+    }
+
+    // MARK: - Actions
+
+    @IBAction private func shapedButtonPressed(_ sender: Any) {
+        guard let _ = scriptTag else {
+            presentAlert("Enter a valid script tag")
+            return
+        }
+        guard let _ = languageTag else {
+            presentAlert("Enter a valid language tag")
+            return
+        }
+
+        performSegue(withIdentifier: SegueID.showInfo, sender: nil)
+    }
+
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         switch segue.identifier {
         case SegueID.showTypefaces:
-            guard let typefacesViewController = segue.destination as? AvailableTypefacesViewController else {
-                return
-            }
-
+            let typefacesViewController = segue.destination as! AvailableTypefacesViewController
             typefacesViewController.setup(selectedTypeface: selectedTypeface, onSelectionChanged: {
                 self.selectedTypeface = typefacesViewController.selectedTypeface
                 self.updateTypefaceTitle()
             })
+
+        case SegueID.showInfo:
+            let infoViewController = segue.destination as! OpenTypeInfoViewController
+            infoViewController.setup(typeface: selectedTypeface,
+                                     typeSize: typeSize,
+                                     scriptTag: scriptTag,
+                                     languageTag: languageTag,
+                                     sourceText: inputText)
 
         default:
             break
