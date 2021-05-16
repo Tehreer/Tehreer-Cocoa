a5406cce3b0845cdd6febedc3fa92ef1f119b87f
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2021-05-16 19:46:58 +0500

    [lib] Introduced intermediate class to wrap harfbuzz face

---
+++ Source/Graphics/ShapableFace.swift
@@ -0,0 +1,179 @@
+//
+// Copyright (C) 2021 Muhammad Tayyab Akram
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+import FreeType
+import HarfBuzz
+
+class ShapableFace {
+    private let renderableFace: RenderableFace
+
+    var hbFont: OpaquePointer!
+
+    init(renderableFace: RenderableFace) {
+        self.renderableFace = renderableFace
+
+        let object = Unmanaged.passUnretained(self).toOpaque()
+        let ftFace = renderableFace.ftFace
+
+        let hbFace = hb_face_create_for_tables({ (face, tag, object) -> OpaquePointer? in
+            let unmanaged = Unmanaged<ShapableFace>.fromOpaque(object!)
+            let instance = unmanaged.takeUnretainedValue()
+
+            return instance.withFreeTypeFace { (ftFace) -> OpaquePointer? in
+                var length: FT_ULong = 0
+                FT_Load_Sfnt_Table(ftFace, FT_ULong(tag), 0, nil, &length)
+
+                guard length > 0 else {
+                    return nil
+                }
+
+                let buffer = UnsafeMutablePointer<FT_Byte>.allocate(capacity: Int(length))
+                FT_Load_Sfnt_Table(ftFace, FT_ULong(tag), 0, buffer, nil)
+
+                return hb_blob_create(UnsafeRawPointer(buffer).assumingMemoryBound(to: Int8.self),
+                                      UInt32(length),
+                                      HB_MEMORY_MODE_WRITABLE,
+                                      nil,
+                                      { $0?.deallocate() })
+            }
+        }, object, nil)
+
+        hb_face_set_index(hbFace, UInt32(ftFace.pointee.face_index))
+        hb_face_set_upem(hbFace, UInt32(ftFace.pointee.units_per_EM))
+
+        let funcs = hb_font_funcs_create()
+
+        hb_font_funcs_set_nominal_glyph_func(funcs, { (font, object, unicode, glyph, userData) -> hb_bool_t in
+            let unmanaged = Unmanaged<ShapableFace>.fromOpaque(object!)
+            let instance = unmanaged.takeUnretainedValue()
+
+            return instance.withFreeTypeFace { (ftFace) in
+                let codePoint = FT_ULong(unicode)
+                let glyphID = FT_Get_Char_Index(ftFace, codePoint)
+
+                guard glyphID > 0 else {
+                    return 0
+                }
+
+                glyph?.initialize(to: hb_codepoint_t(glyphID))
+
+                return 1
+            }
+        }, nil, nil)
+
+        hb_font_funcs_set_nominal_glyphs_func(funcs, { (font, object, count, firstUnicode, unicodeStride, firstGlyph, glyphStride, userData) -> UInt32 in
+            let unmanaged = Unmanaged<ShapableFace>.fromOpaque(object!)
+            let instance = unmanaged.takeUnretainedValue()
+
+            return instance.withFreeTypeFace { (ftFace) in
+                var unicodePtr = UnsafeRawPointer(firstUnicode)!
+                var glyphPtr = UnsafeMutableRawPointer(firstGlyph)!
+                var done: UInt32 = 0
+
+                for _ in 0 ..< count {
+                    let unicodeRef = unicodePtr.assumingMemoryBound(to: hb_codepoint_t.self)
+                    let glyphRef = glyphPtr.assumingMemoryBound(to: hb_codepoint_t.self)
+
+                    let codePoint = FT_ULong(unicodeRef.pointee)
+                    let glyphID = FT_Get_Char_Index(ftFace, codePoint)
+
+                    guard glyphID > 0 else {
+                        break
+                    }
+
+                    glyphRef.initialize(to: glyphID)
+
+                    unicodePtr = unicodePtr.advanced(by: Int(unicodeStride))
+                    glyphPtr = glyphPtr.advanced(by: Int(glyphStride))
+                    done += 1
+                }
+
+                return done
+            }
+        }, nil, nil)
+
+        hb_font_funcs_set_variation_glyph_func(funcs, { (font, object, unicode, variationSelector, glyph, userData) -> hb_bool_t in
+            let unmanaged = Unmanaged<ShapableFace>.fromOpaque(object!)
+            let instance = unmanaged.takeUnretainedValue()
+
+            return instance.withFreeTypeFace { (ftFace) in
+                let glyphID = FT_Face_GetCharVariantIndex(ftFace, FT_ULong(unicode), FT_ULong(variationSelector))
+
+                guard glyphID > 0 else {
+                    return 0
+                }
+
+                glyph?.initialize(to: hb_codepoint_t(glyphID))
+
+                return 1
+            }
+        }, nil, nil)
+
+        hb_font_funcs_set_glyph_h_advance_func(funcs, { (font, object, glyph, userData) -> hb_position_t in
+            let unmanaged = Unmanaged<ShapableFace>.fromOpaque(object!)
+            let instance = unmanaged.takeUnretainedValue()
+
+            return instance.withFreeTypeFace { (ftFace) in
+                let loadFlags = FT_Int32(FT_LOAD_NO_SCALE)
+                var advance: FT_Fixed = 0
+
+                FT_Get_Advance(ftFace, FT_UInt(glyph), loadFlags, &advance)
+
+                return hb_position_t(advance)
+            }
+        }, nil, nil)
+
+        hb_font_funcs_set_glyph_h_advances_func(funcs, { (font, object, count, firstGlyph, glyphStride, firstAdvance, advanceStride, userData) in
+            let unmanaged = Unmanaged<ShapableFace>.fromOpaque(object!)
+            let instance = unmanaged.takeUnretainedValue()
+
+            instance.withFreeTypeFace { (ftFace) in
+                var glyphPtr = UnsafeRawPointer(firstGlyph)!
+                var advancePtr = UnsafeMutableRawPointer(firstAdvance)!
+
+                let loadFlags = FT_Int32(FT_LOAD_NO_SCALE)
+                var advance: FT_Fixed = 0
+
+                for _ in 0 ..< count {
+                    let glyphRef = glyphPtr.assumingMemoryBound(to: hb_codepoint_t.self)
+                    let advanceRef = advancePtr.assumingMemoryBound(to: hb_position_t.self)
+
+                    let glyph = FT_UInt(glyphRef.pointee)
+
+                    FT_Get_Advance(ftFace, glyph, loadFlags, &advance)
+                    advanceRef.initialize(to: hb_position_t(advance))
+
+                    glyphPtr = glyphPtr.advanced(by: Int(glyphStride))
+                    advancePtr = advancePtr.advanced(by: Int(advanceStride))
+                }
+            }
+        }, nil, nil)
+
+        hb_font_funcs_make_immutable(funcs)
+
+        hbFont = hb_font_create(hbFace)
+        hb_font_set_funcs(hbFont, funcs, object, nil)
+    }
+
+    deinit {
+        hb_font_destroy(hbFont)
+    }
+
+    func withFreeTypeFace<Result>(_ body: (FT_Face) throws -> Result) rethrows -> Result {
+        return try renderableFace.withRawFace(body)
+    }
+}
--- Source/Graphics/Typeface.swift
+++ Source/Graphics/Typeface.swift
@@ -34,7 +34,7 @@ private class Instance {
     var ftSize: FT_Size!
     var ftStroker: FT_Stroker!
 
-    var hbFont: OpaquePointer!
+    var shapableFace: ShapableFace!
 
     private struct Description {
         var familyIndex: Int?
@@ -261,147 +261,7 @@ private class Instance {
     }
 
     private func setupHarfBuzz() {
-        let object = Unmanaged.passUnretained(self).toOpaque()
-        let ftFace = renderableFace.ftFace
-
-        let hbFace = hb_face_create_for_tables({ (face, tag, object) -> OpaquePointer? in
-            let unmanaged = Unmanaged<Instance>.fromOpaque(object!)
-            let instance = unmanaged.takeUnretainedValue()
-
-            return instance.withFreeTypeFace { (ftFace) -> OpaquePointer? in
-                var length: FT_ULong = 0
-                FT_Load_Sfnt_Table(ftFace, FT_ULong(tag), 0, nil, &length)
-
-                guard length > 0 else {
-                    return nil
-                }
-
-                let buffer = UnsafeMutablePointer<FT_Byte>.allocate(capacity: Int(length))
-                FT_Load_Sfnt_Table(ftFace, FT_ULong(tag), 0, buffer, nil)
-
-                return hb_blob_create(UnsafeRawPointer(buffer).assumingMemoryBound(to: Int8.self),
-                                      UInt32(length),
-                                      HB_MEMORY_MODE_WRITABLE,
-                                      nil,
-                                      { $0?.deallocate() })
-            }
-        }, object, nil)
-
-        hb_face_set_index(hbFace, UInt32(ftFace.pointee.face_index))
-        hb_face_set_upem(hbFace, UInt32(ftFace.pointee.units_per_EM))
-
-        let funcs = hb_font_funcs_create()
-
-        hb_font_funcs_set_nominal_glyph_func(funcs, { (font, object, unicode, glyph, userData) -> hb_bool_t in
-            let unmanaged = Unmanaged<Instance>.fromOpaque(object!)
-            let instance = unmanaged.takeUnretainedValue()
-
-            return instance.withFreeTypeFace { (ftFace) in
-                let codePoint = FT_ULong(unicode)
-                let glyphID = FT_Get_Char_Index(ftFace, codePoint)
-
-                guard glyphID > 0 else {
-                    return 0
-                }
-
-                glyph?.initialize(to: hb_codepoint_t(glyphID))
-
-                return 1
-            }
-        }, nil, nil)
-
-        hb_font_funcs_set_nominal_glyphs_func(funcs, { (font, object, count, firstUnicode, unicodeStride, firstGlyph, glyphStride, userData) -> UInt32 in
-            let unmanaged = Unmanaged<Instance>.fromOpaque(object!)
-            let instance = unmanaged.takeUnretainedValue()
-
-            return instance.withFreeTypeFace { (ftFace) in
-                var unicodePtr = UnsafeRawPointer(firstUnicode)!
-                var glyphPtr = UnsafeMutableRawPointer(firstGlyph)!
-                var done: UInt32 = 0
-
-                for _ in 0 ..< count {
-                    let unicodeRef = unicodePtr.assumingMemoryBound(to: hb_codepoint_t.self)
-                    let glyphRef = glyphPtr.assumingMemoryBound(to: hb_codepoint_t.self)
-
-                    let codePoint = FT_ULong(unicodeRef.pointee)
-                    let glyphID = FT_Get_Char_Index(ftFace, codePoint)
-
-                    guard glyphID > 0 else {
-                        break
-                    }
-
-                    glyphRef.initialize(to: glyphID)
-
-                    unicodePtr = unicodePtr.advanced(by: Int(unicodeStride))
-                    glyphPtr = glyphPtr.advanced(by: Int(glyphStride))
-                    done += 1
-                }
-
-                return done
-            }
-        }, nil, nil)
-
-        hb_font_funcs_set_variation_glyph_func(funcs, { (font, object, unicode, variationSelector, glyph, userData) -> hb_bool_t in
-            let unmanaged = Unmanaged<Instance>.fromOpaque(object!)
-            let instance = unmanaged.takeUnretainedValue()
-
-            return instance.withFreeTypeFace { (ftFace) in
-                let glyphID = FT_Face_GetCharVariantIndex(ftFace, FT_ULong(unicode), FT_ULong(variationSelector))
-
-                guard glyphID > 0 else {
-                    return 0
-                }
-
-                glyph?.initialize(to: hb_codepoint_t(glyphID))
-
-                return 1
-            }
-        }, nil, nil)
-
-        hb_font_funcs_set_glyph_h_advance_func(funcs, { (font, object, glyph, userData) -> hb_position_t in
-            let unmanaged = Unmanaged<Instance>.fromOpaque(object!)
-            let instance = unmanaged.takeUnretainedValue()
-
-            return instance.withFreeTypeFace { (ftFace) in
-                let loadFlags = FT_Int32(FT_LOAD_NO_SCALE)
-                var advance: FT_Fixed = 0
-
-                FT_Get_Advance(ftFace, FT_UInt(glyph), loadFlags, &advance)
-
-                return hb_position_t(advance)
-            }
-        }, nil, nil)
-
-        hb_font_funcs_set_glyph_h_advances_func(funcs, { (font, object, count, firstGlyph, glyphStride, firstAdvance, advanceStride, userData) in
-            let unmanaged = Unmanaged<Instance>.fromOpaque(object!)
-            let instance = unmanaged.takeUnretainedValue()
-
-            instance.withFreeTypeFace { (ftFace) in
-                var glyphPtr = UnsafeRawPointer(firstGlyph)!
-                var advancePtr = UnsafeMutableRawPointer(firstAdvance)!
-
-                let loadFlags = FT_Int32(FT_LOAD_NO_SCALE)
-                var advance: FT_Fixed = 0
-
-                for _ in 0 ..< count {
-                    let glyphRef = glyphPtr.assumingMemoryBound(to: hb_codepoint_t.self)
-                    let advanceRef = advancePtr.assumingMemoryBound(to: hb_position_t.self)
-
-                    let glyph = FT_UInt(glyphRef.pointee)
-
-                    FT_Get_Advance(ftFace, glyph, loadFlags, &advance)
-                    advanceRef.initialize(to: hb_position_t(advance))
-
-                    glyphPtr = glyphPtr.advanced(by: Int(glyphStride))
-                    advancePtr = advancePtr.advanced(by: Int(advanceStride))
-                }
-            }
-        }, nil, nil)
-
-        hb_font_funcs_make_immutable(funcs)
-
-        hbFont = hb_font_create(hbFace)
-        hb_font_set_funcs(hbFont, funcs, object, nil)
+        shapableFace = ShapableFace(renderableFace: renderableFace)
     }
 
     private func setupAxes(nameTable: NameTable?) {
@@ -539,8 +399,6 @@ private class Instance {
     }
 
     deinit {
-        hb_font_destroy(hbFont)
-
         withFreeTypeFace { _ -> Void in
             FT_Done_Size(ftSize)
         }
@@ -726,7 +584,7 @@ public class Typeface {
     }
 
     var hbFont: OpaquePointer {
-        return instance.hbFont
+        return instance.shapableFace.hbFont
     }
 
     var ftColors: [FT_Color] {
