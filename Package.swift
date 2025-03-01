aec991e9c824f2d2d96fcd87c2cd27d9a6a4c705
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2025-03-01 17:47:37 +0500

    [demo] Rename demo target to highlight UIKit based implementation

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "TehreerCocoa",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "TehreerCocoa",
